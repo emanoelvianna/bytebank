d33645f30b4d9c0e798c83bed1d8512e425f8321
Emanoel Vianna <emanoel_fabiano@sicredi.com.br>
2023-05-02 11:21:25 -0300

    main - add spring

--- src/main/java/br/com/bytebank/Principal.java
+++ src/main/java/br/com/bytebank/Principal.java
@@ -1,56 +1,14 @@
 package br.com.bytebank;
 
 
-import br.com.bytebank.modelo.*;
-import br.com.bytebank.servico.ContaServico;
-import br.com.bytebank.servico.IRServico;
-import br.com.bytebank.servico.ProdutoServico;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-
+@SpringBootApplication
 public class Principal {
 
     public static void main(String[] args) {
-        Cliente emanoel = new Cliente("Emanoel Vianna",
-                "142.135.090-44",
-                LocalDate.of(1993, 1, 22),
-                "91270-010",
-                "vianna@gmail.com", "laranja123");
-        Conta conta = new ContaCorrente(emanoel);
-        conta.setSaldo(new BigDecimal(1000)); // TODO: REMOVER MÉTODO SET
-
-        System.out.println(conta.toString());
-
-        ContaServico servico = new ContaServico();
-        servico.depositar(emanoel, conta, new BigDecimal(100));
-
-        System.out.println(conta.toString());
-
-        Gerente gerente = new Gerente("Pedro Silva",
-                "896.933.390-88",
-                new BigDecimal(5000),
-                "laranja123");
-
-        IRServico irServico = new IRServico();
-        irServico.calcularIR(gerente);
-
-        ProdutoServico produtoServico = new ProdutoServico();
-        Consignado consignado = new Consignado(new BigDecimal(500), new BigDecimal(500), "Matera");
-        Produto<Consignado> pConsignado = new Produto<>("Consignado", consignado);
-        produtoServico.adicionarProduto(pConsignado);
-
-        Finciamento finciamento = new Finciamento(new BigDecimal(500), new BigDecimal(200));
-        Produto<Finciamento> pFinciamento = new Produto<>("Finciamento", finciamento);
-        produtoServico.adicionarProduto(pFinciamento);
-
-        Credito credito = new Credito(new BigDecimal(500), new BigDecimal(200));
-        Produto<Credito> pCredito = new Produto<>("Credito", credito);
-        produtoServico.adicionarProduto(pCredito);
+        SpringApplication.run(Principal.class, args);
     }
 
 }
--- src/main/java/br/com/bytebank/modelo/Operador.java
+++ src/main/java/br/com/bytebank/modelo/Operador.java
@@ -12,9 +12,9 @@ public class Operador extends Funcionario {
 
     @Override
     public int compareTo(Funcionario funcionario) {
-        if(this.getSalario().compareTo(funcionario.getSalario()) < 0) {
+        if (this.getSalario().compareTo(funcionario.getSalario()) < 0) {
             return -1;
-        } else if(this.getSalario().compareTo(funcionario.getSalario()) > 0) {
+        } else if (this.getSalario().compareTo(funcionario.getSalario()) > 0) {
             return 1;
         }
         return 0;
