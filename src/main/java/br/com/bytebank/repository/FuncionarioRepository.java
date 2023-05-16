fa32e1ecbd05d4e503b90396ee88b5adfbe56778
Lucio <lucioerico@gmail.com>
2023-05-16 12:00:57 -0300

    [aula 16-05][resposta]

--- src/main/java/br/com/bytebank/repository/FuncionarioRepository.java
+++ src/main/java/br/com/bytebank/repository/FuncionarioRepository.java
@@ -1,8 +1,25 @@
 package br.com.bytebank.repository;
 
+import br.com.bytebank.modelo.Conta;
+import br.com.bytebank.modelo.Funcionario;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
 public interface FuncionarioRepository {
 
-    // TODO: DENIR CONSULTA PARA BUSCAR FUNCIONARIO POR DOCUMENTO
+    // TODO: DEFINIR CONSULTA PARA BUSCAR FUNCIONARIO POR DOCUMENTO
     // TODO: LISTAR FUNCIONARIOS PELO SALARIO
+
+    @Query("SELECT f FROM Funcionario f WHERE f.documento = :documento")
+    Funcionario findByDocumento(@Param("documento") String documento);
+
+    @Query("SELECT f FROM Funcionario f WHERE f.salario = :salario")
+    List<Funcionario> findBySalario(@Param("salario") BigDecimal salario);
+
 
 }
